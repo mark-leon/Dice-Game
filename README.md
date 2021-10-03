e7e0b263f832a7e9510cee3c5e4fa8feb1dbb035
kamruzzaman leion <mrkleon0004@gmail.com>
2021-10-03 16:09:40 +0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Dice-Game
+# Leon's Dice-Game
+
+In this example game, I have made a simple dice game. The main logic of this game is to generate two random number in javascript. The random number is between 1 to 6. There are totoal 6 images in this project. Based on the random number between 1 to 6, two random image source of dice will be generated and displayed in the browser. If first random number is greater than the second, then player 1 is winner.
+If first random number is less than the second, then player 2 is winner.
+If first random number is equal to the second, then the game is draw.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
@@ -17,9 +17,8 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
-//If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
